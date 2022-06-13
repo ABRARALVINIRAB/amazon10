18d13fa9857032aebd0cea5ae6c7bf5addc57200
abraralvinirab <abrar15-9800@diu.edu.bd>
2022-06-13 17:29:22 +0600

    image add

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "amazon10db": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
